2e263135425fa09a307302a7b80b978388ff705f
e <39879816+hkanyeeee@users.noreply.github.com>
2019-03-07 11:06:32 +0800

    update

--- main.js
+++
@@ -1,3 +0,0 @@
-fetch('http://127.0.0.1:5500/output2.html')
-    .then(res => console.log(res.getAllResponseHeader('Date'))
-    )
--- test.js
+++
@@ -1,4 +0,0 @@
-var a = 11
-var b = 12
-var c = a + b
-console.log(c)
