be6f06bd0c434ffad50bc039329eac6425110905
e <39879816+hkanyeeee@users.noreply.github.com>
2019-05-14 16:55:47 +0800

    update

--- main.py
+++ main.py
@@ -21,17 +21,21 @@ def hello(pager):
     viceWenzhangData = []
     doubanBookData = []
     doubanMovieData = []
-    frontEndData = []
-    androidData = []
-    backEndData = []
+    v2exAllData = []
+    v2exTechData = []
+    # frontEndData = []
+    # androidData = []
+    # backEndData = []
     url1 = 'https://www.gcores.com/categories/1/originals?page=%s' % (pager)
     url2 = 'https://www.gcores.com/categories/2/originals?page=%s' % (pager)
     url3 = 'http://www.vice.cn/articles/page/%s' % (pager)
     url4 = 'https://movie.douban.com/review/best/?start=40'
     url5 = 'https://book.douban.com/review/best/?start=40'
-    url6 = 'https://juejin.im/welcome/frontend'
-    url7 = 'https://juejin.im/welcome/android'
-    url8 = 'https://juejin.im/welcome/backend'
+    url6 = 'https://www.v2ex.com/?tab=all'
+    url7 = 'https://www.v2ex.com/?tab=tech'
+    # url6 = 'https://juejin.im/welcome/frontend'
+    # url7 = 'https://juejin.im/welcome/android'
+    # url8 = 'https://juejin.im/welcome/backend'
     if int(pager) < 4:
         url4 = 'https://movie.douban.com/review/best/?start=%d' % ((int(pager) - 1)*20)
         url5 = 'https://book.douban.com/review/best/?start=%d' % ((int(pager) - 1)*20)
@@ -42,13 +46,13 @@ def hello(pager):
     e = requests.get(url5)
     f = requests.get(url6)
     g = requests.get(url7)
-    h = requests.get(url8)
+    # h = requests.get(url8)
 
     # 处理机核Data
     def getmakeJiheList(html, data):
         soup = BeautifulSoup(html, "html.parser")
         divData = soup.find_all(class_ = 'showcase_text')
-        print(type(divData))
+        # print(type(divData))
         for oneData in divData:
             if len(oneData.select('a')) > 0:
                 data.append({
@@ -61,7 +65,7 @@ def hello(pager):
     def getmakeViceList(html, data):
         soup = BeautifulSoup(html, "html.parser")
         divData = soup.find_all(class_ = 'entry-title')
-        print(type(divData))
+        # print(type(divData))
         for oneData in divData:
             if len(oneData.select('a')) > 0:
                 data.append({
@@ -84,9 +88,21 @@ def hello(pager):
         return data
 
     # 处理掘金 Data
-    def getjuejinList(html, data):
+    # def getjuejinList(html, data):
+    #     soup = BeautifulSoup(html, "html.parser")
+    #     print(soup)
+    #     divData = soup.find_all(class_ = 'info-row title-row')
+    #     for oneData in divData:
+    #         if len(oneData.select('a')[0]) > 0:
+    #             data.append({
+    #                     'articalName': oneData.select('a')[0].string,
+    #                     'articalHref': oneData.select('a')[0]['href']
+    #                 })
+    #     return data
+
+    def getV2exList(html, data):
         soup = BeautifulSoup(html, "html.parser")
-        divData = soup.find_all(class_ = 'info-row title-row')
+        divData = soup.find_all(class_ = 'item_title')
         for oneData in divData:
             if len(oneData.select('a')[0]) > 0:
                 data.append({
@@ -101,9 +117,11 @@ def hello(pager):
         'viceWenzhang': getmakeViceList(c.text, viceWenzhangData),
         'doubanMovieData': getmakeDoubanList(d.text, doubanMovieData),
         'doubanBook': getmakeDoubanList(e.text, doubanBookData),
-        'frontEnd' : getjuejinList(f.text, frontEndData),
-        'android' : getjuejinList(g.text, androidData),
-        'backEnd' : getjuejinList(h.text, backEndData)
+        'v2exAll': getV2exList(f.text, v2exAllData),
+        'v2exTech': getV2exList(g.text, v2exTechData)
+        # 'frontEnd' : getjuejinList(f.text, frontEndData),
+        # 'android' : getjuejinList(g.text, androidData),
+        # 'backEnd' : getjuejinList(h.text, backEndData)
         })
 
 
